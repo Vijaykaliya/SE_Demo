9d5b29f3be9400644cfbd61e5accafcd006ee584
Vijaykaliya <bbkaliya5981@gmail.com>
2025-09-15 20:49:39 +0530

    commit

---
+++ 23-8/main (3).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+     int a = 10;  // variable declaration
+     
+     printf("Your Value is %d",a);  //print variable Value
+     
+     getch();
+ }
